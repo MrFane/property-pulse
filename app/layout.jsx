c70cfa5e81390f805d70f1660ad8f0c9e5b56014
DataStreamViking <stedzh99@gmail.com>
2024-02-28 19:59:29 +0100

    day 2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,19 +1,23 @@
+import Navbar from "@/components/Navbar";
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 
 export const metadata = {
-    title: "PropertyPulse | Find the Perfect Rental",
-    description: "Find your dream rental property",
-    keywords: "rental, find rentals, find properties",
+  title: "PropertyPulse | Find the Perfect Rental",
+  description: "Find your dream rental property",
+  keywords: "rental, find rentals, find properties",
 };
 
 const MainLayout = ({ children }) => {
-    return (
-        <html>
-            <body>
-                <div>{children}</div>
-            </body>
-        </html>
-    );
+  return (
+    <html>
+      <body>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
 };
 
 export default MainLayout;
